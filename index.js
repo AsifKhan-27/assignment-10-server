b68f98fa3f64accd3097bf230368694691abab09
AsifKhan-27 <asifak001@gmail.com>
2023-01-02 02:48:37 +0600

    added nodemon

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const port= process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories=require('./data/categories.json');
 
+app.get('/', (req, res)=>{
+    res.send('API is Running ');
+});
 
 app.listen(port, ()=>{
     console.log('Dragon News server runnung on port', port);
