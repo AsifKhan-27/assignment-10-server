34072c0562bd65a45698f6c0c0d2c34870476899
AsifKhan-27 <asifak001@gmail.com>
2023-01-02 03:30:22 +0600

    added details data

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get('/', (req, res)=>{
     res.send('API is Running ');
 });
 
+app.get('/categories', (req, res)=>{
+    res.send(categories);
+})
+
 app.listen(port, ()=>{
     console.log('Dragon News server runnung on port', port);
 })
